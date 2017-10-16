a37ac2326b32558855749592ec86abd58b6c8569
Zsolt Toth <tothzs87@gmail.com>
2017-10-16 09:37:56 +0200

    css has been added

--- spring-web/src/main/java/hu/uni/miskolc/iit/webdev/spring/web/config/SpringWebExampleConfiguration.java
+++ spring-web/src/main/java/hu/uni/miskolc/iit/webdev/spring/web/config/SpringWebExampleConfiguration.java
@@ -4,6 +4,9 @@ import hu.uni.miskolc.iit.webdev.spring.web.controller.CalculatorController;
 import hu.uni.miskolc.iit.webdev.spring.web.controller.GreetingsController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 /**
  * Created by tothzs on 2017.10.16..
@@ -20,5 +23,14 @@ public class SpringWebExampleConfiguration {
     @Bean
     public CalculatorController calculatorController(){
         return new CalculatorController();
+    }
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver result = new InternalResourceViewResolver();
+        result.setViewClass(JstlView.class);
+        result.setPrefix("/WEB-INF/views");
+        result.setSuffix(".html");
+        return result;
     }
 }
--- spring-web/src/main/webapp/index.jsp
+++ spring-web/src/main/webapp/index.jsp
@@ -1,4 +1,7 @@
 <html>
+<head>
+    <link rel="stylesheet" href="/css/stlye.css" type="text/css"/>
+</head>
 <body>
 <h2>Hello World!</h2>
 </body>
