a7101ea7b315da0ea388a8bf97ec86dbddccb7a1
Zsolt Toth <tothzs87@gmail.com>
2017-11-26 20:09:25 +0100

    cars - #4 CarPrototypeManagerServiceImpl.java has been added. CarPrototypeDAO.java has been defined CarDAO has been extended with search by plate number method.

---
+++ cars/service/src/main/java/hu/uni/miskolc/iit/webdevelopment/examples/cars/dao/exceptions/PrototypeIDAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package hu.uni.miskolc.iit.webdevelopment.examples.cars.dao.exceptions;
+
+/**
+ * Created by tothzs on 2017.11.24..
+ */
+public class PrototypeIDAlreadyExistsException extends Exception {
+}
--- cars/service/src/main/java/hu/uni/miskolc/iit/webdevelopment/examples/cars/service/CarManagerServiceImpl.java
+++ cars/service/src/main/java/hu/uni/miskolc/iit/webdevelopment/examples/cars/service/CarManagerServiceImpl.java
@@ -3,13 +3,17 @@ package hu.uni.miskolc.iit.webdevelopment.examples.cars.service;
 import hu.uni.miskolc.iit.webdevelopment.examples.cars.dao.CarDAO;
 import hu.uni.miskolc.iit.webdevelopment.examples.cars.dao.exceptions.CarRecordAlreadyExistsException;
 import hu.uni.miskolc.iit.webdevelopment.examples.cars.model.Car;
+import hu.uni.miskolc.iit.webdevelopment.examples.cars.model.CarProducer;
+import hu.uni.miskolc.iit.webdevelopment.examples.cars.model.CarPrototype;
+import hu.uni.miskolc.iit.webdevelopment.examples.cars.model.exceptions.InvalidProductionDateExeption;
 import hu.uni.miskolc.iit.webdevelopment.examples.cars.service.CarManagerService;
 import hu.uni.miskolc.iit.webdevelopment.examples.cars.service.exceptions.ExistingPlateNumber;
 
+import java.awt.*;
 import java.util.Collection;
+import java.util.Date;
 
 /**
- * Hello world!
  *
  */
 public class CarManagerServiceImpl implements CarManagerService
@@ -24,8 +28,8 @@ public class CarManagerServiceImpl implements CarManagerService
         return dao.readCars();
     }
 
-    public Collection<Car> listCarsbyBrand(String brand) {
-        return dao.readCarsByBrand(brand);
+    public Collection<Car> listCarsbyBrand(CarProducer producer) {
+        return dao.readCars(producer);
     }
 
     public void recordCar(Car car) throws ExistingPlateNumber {
@@ -40,5 +44,10 @@ public class CarManagerServiceImpl implements CarManagerService
             //Sophisticated Exception Handling
             throw new ExistingPlateNumber(e);
         }
+    }
+
+    public void recordCar(String plateNo, CarPrototype prototype, Color color, Date productionDate) throws ExistingPlateNumber, InvalidProductionDateExeption {
+        Car car = new Car(plateNo, prototype, color,productionDate);
+        this.recordCar(car);
     }
 }
